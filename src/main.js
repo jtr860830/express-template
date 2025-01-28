d374c3fa54591daae99bffa98b085149b30bba06
Josh Hsieh <josh.hsieh@linux.com>
2025-01-28 19:27:34 +0800

    chore: add some tips

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import express from "express";
 import logger from "pino-http";
+import config from "config";
 
 import errorHandler from "./middleware/error_handler.js";
 
 const app = express();
+const host = config.get("host");
+const port = config.get("port");
 
 app.use(express.json());
 app.use(logger());
@@ -12,8 +15,9 @@ app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
+// error handler should be the last middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+app.listen(port, host, () => {
+	console.info({ msg: `server is running on http://${host}:${port}` });
 });
